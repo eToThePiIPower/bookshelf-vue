4f3c7790a33299ea38845efa008283f90e9b6696
Charles Beynon <eToThePiIPower@gmail.com>
2019-10-11 16:43:58 -0400

    Implement JWT refresh
    With the RefreshController implemented on the back-end server we can now
    implement refreshing expired access tokens in the front-end
    application..
    
    JWT access tokens have a 1-hour expiration time, with the capability to
    refresh the token over a 2-week period. In theory this could be
    implemented with a separate refresh token, but the current back-end
    simply uses the (recently expired) access token to receive a fresh one
    with a new 1-hour expiration window.
    
    The refresh action is implemented by tapping into the axios intercept of
    the back-end response and looking for 401 Authentication errors. Instead
    of immediately redirecting to the signin page, a refresh is attempted.
    
    * If the refresh is successful, the previously unauthenticated action is
      reattempted.
    * If the refresh fails, the user is redirected to the login page.
    
    Note: This has not been tested with loops of 401 errors --- that is,
    receiving a 401 for a reason *besides* not being logged in or having an
    expired token. This needs to be investigated.

--- src/backend/axios/index.js
+++ src/backend/axios/index.js
@@ -31,11 +31,21 @@ authConnection.interceptors.request.use(config => {
 
 authConnection.interceptors.response.use(null, error => {
   if (error.response && error.response.config && error.response.status === 401) {
-    delete localStorage.csrf
-    delete localStorage.signedIn
-    // the router isnt defined yet so we need to use an absolute path
-    location.replace('/#/signin')
-    return Promise.reject(error)
+    return plainConnection.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
+      .then(response => {
+        localStorage.csrf = response.data.csrf
+        localStorage.signedIn = true
+        // After another successfull refresh - repeat original request
+        let retryConfig = error.response.config
+        retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf
+        return plainConnection.request(retryConfig)
+      })
+      .catch(error => {
+        delete localStorage.csrf
+        delete localStorage.signedIn
+        this.$router.replace('/signin')
+        return Promise.reject(error)
+      })
   }
   if (error.response && error.response.config && error.response.status >= 400 && error.response.status < 500) {
     return Promise.reject(error)
