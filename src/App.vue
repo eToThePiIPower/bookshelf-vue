96308252a1b465529f6ffda758be1122793560d8
Charles Beynon <eToThePiIPower@gmail.com>
2019-10-12 23:11:16 -0400

    Support multiple error messages at once
    This minor feature update supports multiple error flash messages at
    once, and provides a framework to eventually support different types of
    flash messages besides errors.
    
    The `error` data variable is replaces by an `errors` array, and the
    `setError` function now pushes objects to this array. The Error objects
    contain 4 attributes:
    * timestamp: for deriving keys and ids, and used for eventually
      dismissing particular errors
    * variant: hard-coded to `danger` but in theory the setError mixin can
      be updated to allow other values to be passed
    * text: a brief error description, as provided by the Vue application.
      Displayed in bold and generally a high-level description
    * details: details of the error as provided by the back-end. Will
      generally either provide more specific reasons for the error or a list
      of failed validations
    
    When dismissed, an error will be both faded from view and removed from
    the errors array. This will prevent the array from accumulating too many
    'old' errors in a long-lived single-page application session.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,15 @@
   <div id="app">
     <Navbar/>
 
-    <b-alert variant="warning" v-model="error.text" v-if="!isEmpty(error)" dismissible fade>
+    <b-alert
+      dismissible
+      fade
+      v-for="error in errors"
+      v-bind:key="error.timestamp"
+      v-bind:show="error.timestamp"
+      v-bind:variant="error.variant"
+      @dismissed="dropError(error.timestamp)"
+    >
       <b>{{ error.text }}</b>
       <ul v-if="error.details && (typeof error.details === 'object')">
         <li v-for="key in Object.keys(error.details)" v-bind:key="key">
@@ -27,19 +35,25 @@ export default {
   data () {
     return {
       signedIn: localStorage.signedIn,
-      error: {}
+      errors: []
     }
   },
   components: {
     Navbar
   },
   methods: {
+    dropError (timestamp) {
+      let index = this.errors.findIndex(error => error.timestamp === timestamp)
+      this.errors.splice(index, 1)
+    },
     setError (error, text) {
       var details = (error.response && error.response.data && error.response.data.error)
-      this.error = {
+      this.errors.push({
+        variant: 'danger',
+        timestamp: Date.now(),
         text: text,
         details: details
-      }
+      })
     }
   }
 }
